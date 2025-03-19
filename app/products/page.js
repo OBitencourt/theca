148d364a1696b4fb56068a523fe84a603658207e
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2025-03-19 20:18:04 -0100

    feat: adding initial strucutre of products page with the main structure of item div

--- app/products/page.js
+++ app/products/page.js
@@ -1,10 +1,59 @@
+"use client"
+
 import TemplateDefault from "@/src/templates/TemplateDefault"
+import { ActionButton, ButtonsWrapper, InfoWrapper, Item, ItemsWrapper, Subtitle, Title, TitleWrapper } from "./style"
+import Image from "next/image"
 
 const Products = () => {
 
   return (
     <TemplateDefault>
-      p
+      <Title>
+        Empresa X
+      </Title>
+      <Subtitle>
+        Aqui estão os seus itens e produtos cadastrados:
+      </Subtitle>
+
+      <ItemsWrapper>
+        <Item>
+          <TitleWrapper>
+            <h2>
+              Café
+            </h2>
+          </TitleWrapper>
+          <InfoWrapper>
+            <h4>
+              Quantidade atual: <span>5 unidades</span>
+            </h4>
+            <h4>
+              Validade: <span>31/03/2025</span>
+            </h4>
+            <h4>
+              Fornecedor: <span>Cof&Fee</span>
+            </h4>
+          </InfoWrapper>
+          <ButtonsWrapper>
+            <ActionButton>
+              <Image 
+                src="/images/edit-icon.svg"
+                alt="Edit"
+                width={35}
+                height={35}
+              />
+            </ActionButton>
+            <ActionButton>
+              <Image 
+                src="/images/trash-icon.svg"
+                alt="Edit"
+                width={35}
+                height={35}
+              />
+            </ActionButton>
+            
+          </ButtonsWrapper>
+        </Item>
+      </ItemsWrapper>
     </TemplateDefault>
   )
 }
--- src/templates/TemplateDefault/index.js
+++ src/templates/TemplateDefault/index.js
@@ -5,7 +5,7 @@ import Sidebar from '@/src/components/Sidebar'
 import styled from 'styled-components'
 
 const MainContent = styled.div`
-    padding: 30px 0 0 20%;
+    padding: 20px 0 0 20%;
     width: 100%;
     box-sizing: border-box;
 `
